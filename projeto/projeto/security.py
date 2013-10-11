724427cdac4769f1973f8f14a6bb16ce98db4e4b
Andres MRM <andresmrm@gmail.com>
2013-10-11 16:13:22 -0300

    arrumando edentificacao de usuario/grupo

--- projeto/projeto/security.py
+++ projeto/projeto/security.py
@@ -1,44 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import time
 
-from .models import (
-    BdJogador,
-    )
-
-CACHE = {}
-
-
-def groupfinder(nome, request):
+def groupfinder(identificador, request):
     grupos = []
 
-    # jogador
-    tempo = CACHE.get(nome)
-    if not tempo or (time.time()-tempo) > 60:
-        jog = request.db.query(BdJogador).filter_by(nome=nome).first()
-        if jog:
-            CACHE[nome] = time.time()
-    else:
-        jog = True
-    if jog:
-        grupos.append('g:jogador')
-
-        # desafio
-        if jog is True:
-            jog = request.db.query(BdJogador).filter_by(nome=nome).first()
-        tipo = request.matchdict.get('tipo')
-        if tipo:
-            tipos_destravados = jog.desafios_destravados.split(",")
-            if tipo in tipos_destravados:
-                grupos.append("g:desafio")
-
-    request.jogador = jog
-
-    try:
-        if nome in ['a']:
-            grupos.append("g:analise")
-    except:
-        pass
+    cid = request.db['cidadaos'].get(identificador)
+    request.cidadao = cid
+    if cid:
+        grupos.append('g:cidadao')
 
     return grupos
