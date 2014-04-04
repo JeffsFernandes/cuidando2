115a635041d64323f12ac23042631704400f270d
renansfs <renansfs@gmail.com>
2014-04-04 14:54:41 -0300

    Site provisório

---
+++ creches/Site/cuidando/header.inc.php
@@ -0,0 +1,28 @@
+<div class="navbar navbar-fixed-top">
+	<div class="navbar-inner">
+		<div class="container">
+			<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</a>
+			<a class="brand" href="index.php">Cuidando da Minha Creche</a>
+			<div class="nav-collapse" id="main-menu">
+				<ul class="nav">
+					<li><a href="index.php">Home</a></li>
+					<li><a href="#about">Sobre</a></li>
+					<li><a href="contact.php">Contato</a></li>
+					
+				</ul>
+				<ul class="nav pull-right">
+					<li>
+						<a target="_blank" href="https://twitter.com/share" data-url="http://www.gpopai.usp.br/cuidando" data-lang="pt">
+							Tweetar <i class="icon-share-alt"></i>
+						</a>
+					</li>
+					<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
